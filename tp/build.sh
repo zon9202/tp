c3c64713080ddbb39243577082d66d68286a2dc1
Admin <zlata.nesterenko01@gmail.com>
2025-02-12 16:36:25 +0200

    Fix build script permissions

--- tp/build.sh
+++ tp/build.sh
@@ -1,6 +1,7 @@
-echo #!/usr/bin/env bash
-echo # exit on error
-echo set -o errexit
-echo pip install -r requirements.txt
-echo python manage.py collectstatic --no-input
-echo python manage.py migrate > build.sh
+echo '#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate' | Out-File -Encoding UTF8 build.sh
