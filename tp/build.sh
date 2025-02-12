0d41983925d7270ffd27652ec425918124722d9b
Admin <zlata.nesterenko01@gmail.com>
2025-02-12 16:28:17 +0200

    Fix line endings in build script

--- tp/build.sh
+++ tp/build.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+echo '#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate' | Out-File -encoding UTF8 build.sh
