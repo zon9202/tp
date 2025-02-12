979de851a19b66e20ab7c04a489182471ec5dbfd
Admin <zlata.nesterenko01@gmail.com>
2025-02-12 17:17:49 +0200

    Update build.sh for Render deployment

--- tp/build.sh
+++ tp/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -o errexit
-
+echo "Python version:"
+python --version
+echo "Pip version:"
+pip --version
 python -m pip install --upgrade pip
-pip install -r requirements.txt
-
+python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
