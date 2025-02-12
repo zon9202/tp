c57f0e94ab7264dd5b97f65a29c9f78537994be3
Admin <zlata.nesterenko01@gmail.com>
2025-02-12 16:54:29 +0200

    Update ALLOWED_HOSTS for Render deployment

--- tp/tp/settings.py
+++ tp/tp/settings.py
@@ -28,11 +28,9 @@ SECRET_KEY = 'django-insecure-^txu8gsg$xlxr76t_-vf$ne3)al-^4(dik1f@*#iip2vc+xz(e
 DEBUG = True
 
 ALLOWED_HOSTS = ['https://tp-3ans.onrender.com', 'localhost', '127.0.0.1']
-
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-# Application definition
 
 INSTALLED_APPS = [
     'main',
